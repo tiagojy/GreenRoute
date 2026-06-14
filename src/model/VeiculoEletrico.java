d79647e4569559e43fdb4afc1e6a9eae4030424f
joao-pedro18 <joaopedrogomesfs18@gmail.com>
2026-06-14 19:51:43 -0300

    Classe VeiculoEletrico

--- src/model/VeiculoEletrico.java
+++ src/model/VeiculoEletrico.java
@@ -2,5 +2,31 @@ public class VeiculoEletrico extends Veiculo {
     private String tipoConector;
     private int tempoRecargaRapida;
 
-    
+    public VeiculoEletrico(int id, String modelo, double autonomiaMaxima, double cargaBateriaAtual, double consumoKwhPorKm, int tempoRecargaCompleta, String tipoConector, int tempoRecargaRapida) {
+
+        super(id, modelo, autonomiaMaxima, cargaBateriaAtual, consumoKwhPorKm, tempoRecargaCompleta);
+
+        this.tipoConector = tipoConector;
+        this.tempoRecargaRapida = tempoRecargaRapida;
+    }
+
+    // MÉTODOS GET PARA CADA ATRIBUTO
+
+    public String getTipoConector() {
+        return tipoConector;
+    }
+
+    public int getTempoRecargaRapida() {
+        return tempoRecargaRapida;
+    }
+
+    // MÉTODOS SET PARA CADA ATRIBUTO
+
+    public void setTipoConector(String tipoConector) {
+        this.tipoConector = tipoConector;
+    }
+
+    public void setTempoRecargaRapida(int tempoRecargaRapida) {
+        this.tempoRecargaRapida = tempoRecargaRapida;
+    }
 }
